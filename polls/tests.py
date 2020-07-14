c4f369f6c571cd6f50199c30883a01713d494049
敖旭扬 <jerryao@mail.ustc.edu.cn>
2020-07-14 20:49:15 +0800

    编写测试代码

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,19 @@
+import datetime
+
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        如果问题在未来被发布，则was_published_recently()返回False
+        """
+        time=timezone.now()+datetime.timedelta(days=30)
+        future_question=Question(pub_date=time)
+        self.assertIs(future_question.was_publisted_recently,False)
