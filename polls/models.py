17dcea69920bb21a60ea1d5dad619a9a3dc78252
敖旭扬 <jerryao@mail.ustc.edu.cn>
2020-07-15 07:44:49 +0800

    编写更多测试

--- polls/models.py
+++ polls/models.py
@@ -4,23 +4,40 @@ from django.db import models
 
 # Create your models here.
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published', auto_now=True)
 
     def __str__(self):
-        return '%s' % self.question_text
+        return self.question_text
 
-    def was_publisted_recently(self):
-        return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
+    def was_published_recently(self):
+        now = timezone.now()
+        return now >= self.pub_date >= now-datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(
+        Question, related_name='choices', on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return '%s(%d)=>%s' % (self.choice_text, self.votes, self.question)
+        return '%s' % self.choice_text
+
+
+class Vote(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(
+        Choice, on_delete=models.CASCADE, related_name='votes')
+    voted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    votes = models.IntegerField(default=1)
+
+    class Meta:
+        unique_together = ("question", "voted_by")
+
+    def __str__(self):
+        return '%s(%d)=>%s[%s]' % (self.voted_by, self.votes, self.choice, self.question)
--- polls/tests.py
+++ polls/tests.py
@@ -9,11 +9,28 @@ from django.utils import timezone
 
 from .models import Question
 
+
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         """
         如果问题在未来被发布，则was_published_recently()返回False
         """
-        time=timezone.now()+datetime.timedelta(days=30)
-        future_question=Question(pub_date=time)
-        self.assertIs(future_question.was_publisted_recently,False)
+        time = timezone.now()+datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        如果问题在1天以前被发布，则was_published_recently()返回False
+        """
+        time = timezone.now()-datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        如果问题在最近1天内被发布，则was_published_recently()返回True
+        """
+        time = timezone.now()-datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
